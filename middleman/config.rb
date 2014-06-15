724bba9a10dfc27e59a90986e900a52da51cb9c8
Steven J. Dale <sdale@thoughtworks.com>
2014-06-15 01:25:05 -0500

    adding middleman template

---
+++ middleman/config.rb
@@ -0,0 +1,22 @@
+sprockets.append_path File.join "#{root}", "components"
+
+set :css_dir, 'css'
+set :js_dir, 'js'
+set :img, 'images'
+
+compass_config do |config|
+	config.add_import_path "./components"
+end
+
+after_configuration do
+	@bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
+	sprockets.append_path File.join "#{root}", @bower_config["directory"]
+end
+
+configure :build do
+	activate :minify_css
+	activate :minify_javascript
+	activate :relative_assets
+end
+
+
